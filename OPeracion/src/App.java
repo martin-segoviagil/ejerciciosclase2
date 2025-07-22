b4b7b1295f8e0754e73f33e5e9b1e841e2a1a64a
Martin Segovia <martinsegoviagil@gmail.com>
2025-07-22 12:54:58 -0700

    changes

--- OPeracion/src/App.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;   
-
-public class App {
-    public static void main(String[] args) throws Exception {
-   var scanner = new Scanner (system.in);
-var nombre = scanner.nextint();
-var nombre= scanner.nextline();
-    }
-}
