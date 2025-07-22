4176f9e5c8b0d1103fea405a97fa25db366c79cb
Martin Segovia <martinsegoviagil@gmail.com>
2025-07-22 12:54:47 -0700

    changes: solved problem

---
+++ OPeracion/src/Operacion.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;   
+
+public class Operacion {
+    public static void main(String[] args) throws Exception {
+        Scanner entrada = new Scanner(System.in);
+        
+        double result = 6/2*(1+2);
+        System.out.println(result);
+    }
+}
