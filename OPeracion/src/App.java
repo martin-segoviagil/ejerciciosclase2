fb75609ef0a020709a261812e94b6b1d317634bc
Martin Segovia <martinsegoviagil@gmail.com>
2025-07-21 17:43:03 -0700

    first commit

---
+++ OPeracion/src/App.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;   
+
+public class App {
+    public static void main(String[] args) throws Exception {
+   var scanner = new Scanner (system.in);
+var nombre = scanner.nextint();
+var nombre= scanner.nextline();
+    }
+}
